dad39cbee839319fdc9a909f881f450adc0cc2a7
chevyje <jurre.blankers@gmail.com>
2025-07-15 20:27:02 +0200

    Project pages for each project. auto redirect to projects page if no project exists.

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./pages/landingPage.tsx";
 import ContactPage from "./pages/contactPage.tsx";
 import ProjectsPage from "./pages/projectsPage.tsx";
+import ProjectPage from "./pages/projectPage.tsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/contact" element={<ContactPage />} />
                 <Route path={"/projects"} element={<ProjectsPage />} />
+                <Route path={"/project/:id"} element={<ProjectPage />} />
             </Routes>
         </BrowserRouter>
     </>
--- portfolio/src/pages/projectsPage.tsx
+++ portfolio/src/pages/projectsPage.tsx
@@ -3,15 +3,19 @@ import Footer from "../components/footer.tsx";
 import lang from "../lang/en.json";
 import Style from "./projectsPage.module.css";
 
+import { useNavigate } from "react-router";
+
 function projectsPage() {
     const latestProjects = [...lang.projectsObjects].sort((a, b) => parseDate(b.date).getTime() - parseDate(a.date).getTime());
+    let navigate = useNavigate();
+
     return (
         <>
             <Navbar />
             <div className={Style.projectsContainer}>
                 <div className={Style.projectContainer}>
                     {latestProjects.map((project, index) => (
-                        <div className={Style.project} key={index}>
+                        <div className={Style.project} key={index} onClick={() => navigate(`/project/${project.id}`)}>
                             <div className={Style.projectImage}></div>
                             <div className={Style.projectInfo}>{project.title}</div>
                         </div>
