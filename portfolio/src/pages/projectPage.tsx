989ec3c46d3852d1adcc0ad921f248d7feac61c8
chevyje <jurre.blankers@gmail.com>
2025-07-16 21:22:22 +0200

    Made the completion for the project page

--- portfolio/src/pages/projectPage.tsx
+++ portfolio/src/pages/projectPage.tsx
@@ -1,9 +1,9 @@
 import Navbar from "../components/navbar.tsx";
 import Footer from "../components/footer.tsx";
 import lang from "../lang/en.json"
+import Style from "./projectPage.module.css"
 
 import { useParams, useNavigate } from "react-router";
-
 
 function projectPage() {
     const { id } = useParams();
@@ -12,9 +12,13 @@ function projectPage() {
     if (!project) { navigate("/projects"); return null; }
     return (
         <>
-            <Navbar />
-            <h1>{project.title}</h1>
-            <Footer />
+            <div className={Style.wrapper}>
+                <Navbar />
+                <h1 className={Style.title}>{project.title}</h1>
+                <p className={Style.description}><img src={`/pictures/${project.image}`} alt={project.image} className={Style.image}/>{project.description}</p>
+                <a className={Style.button} style={{ backgroundColor: '#0BB7F6', color: 'white', }} href={"https://www.github.com"} target={"_blank"} rel={"noopener noreferrer"}>{lang.moreButton}</a>
+                <Footer style={{ margin: "auto 0 0 0" }}/>
+            </div>
         </>
     )
 }
