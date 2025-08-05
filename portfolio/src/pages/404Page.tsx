76141026c7fce2e09012d06c341f8a5face9c233
chevyje <jurre.blankers@gmail.com>
2025-08-05 23:27:12 +0200

    Made the last pages responsive for smaller devices

--- portfolio/src/pages/404Page.tsx
+++ portfolio/src/pages/404Page.tsx
@@ -4,10 +4,12 @@ import Footer from "../components/footer.tsx";
 import Style from "./404Page.module.css"
 import {useEffect, useState} from "react";
 import {loadLanguage, useLanguage} from "../scripts/language.ts";
+import {useNavigate} from "react-router";
 
 function ErrorPage() {
     const [lang, setLang] = useState<any>(null);
     const language = useLanguage();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (!language) return;
@@ -16,14 +18,21 @@ function ErrorPage() {
             setLang(langData);
         })();
     }, [language]);
+
+    const toHomePage = () => {
+        navigate(`/${language}`);
+    }
+
     return (
         <>
             {lang && <div className={Style.wrapper}>
                 <Navbar />
-                <h1>{lang.errorPage.title}</h1>
-                <p>{lang.errorPage.subtitle}</p>
-                <p>{lang.errorPage.message}</p>
-                <div>{lang.errorPage.button}</div>
+                <h1 className={Style.title}>{lang.errorPage.title}</h1>
+                <h2 className={Style.subtitle}>{lang.errorPage.subtitle}</h2>
+                <p className={Style.message}>{lang.errorPage.message.split('\n').map((line: any, index: number) => (
+                    <span key={index}>{line}<br/></span>
+                ))}</p>
+                <div className={Style.button} onClick={toHomePage}>{lang.errorPage.button}</div>
                 <Footer style={{ margin: "auto 0 0 0" }}/>
             </div>}
         </>)
